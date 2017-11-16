909fddd52e6ecff8a23b4e4e00d0314619c2d7cf
hyoro-were <choco-la@users.noreply.github.com>
2017-11-16 20:09:19 +0900

    upgrade to use filterResponseData

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 (function() {
   'use strict'
-  const updateNameJs = '/update_name.js'
-
-
   const instances = [
     'https://mstdn.jp/',
     'https://pawoo.net/',
@@ -15,16 +12,57 @@
   }
 
 
-  const listen_toot = (event) => {
-    console.log('onComplete detected')
-    if (event.method === 'POST') {
-      console.log('exec ' + updateNameJs)
-      chrome.tabs.executeScript({file: updateNameJs})
-    }
+  let host = ''
+  let bearer_token = ''
+  let base = ''
+  const assign_from_msg = (msg) => {
+    bearer_token = msg.token
+    host = msg.host
+    base = msg.base
+  }
+  browser.runtime.onMessage.addListener(assign_from_msg)
+
+
+  const update_display_name = (name, token) => {
+    const update = host + 'api/v1/accounts/update_credentials'
+    console.log(update)
+
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('PATCH', update)
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    xhr.timeout = '3000'
+
+    const patch = 'display_name=' + encodeURIComponent(name).replace(/%20/, '+')
+    xhr.send(patch)
   }
 
-  chrome.webRequest.onCompleted.addListener(
+  const decoder = new TextDecoder('utf-8');
+  const listen_toot = (details) => {
+    console.log('onBefore detected')
+    if (details.method === 'POST') {
+      const filter = browser.webRequest.filterResponseData(details.requestId)
+
+      filter.ondata = (event) => {
+        console.log('ondata detected')
+        const resp_str = decoder.decode(event.data, {stream: true})
+        const resp_json = JSON.parse(resp_str)
+        const count = resp_json['account']['statuses_count']
+        const name = base + '[' + String(count) + ']'
+
+        filter.write(event.data)
+        filter.close();
+
+        update_display_name(name, bearer_token)
+      }
+
+      return {}
+    }
+  }
+  chrome.webRequest.onBeforeRequest.addListener(
     listen_toot,
-    {urls: listenUrls}
+    {urls: listenUrls},
+    ['blocking']
   )
 })()
--- init.js
+++ init.js
@@ -5,34 +5,16 @@
   const myNumId = initState.meta['me']
 
 
-  const tootCount = initState.accounts[myNumId]['statuses_count']
   const displayName = initState.accounts[myNumId]['display_name']
   // display_name without toot count.
   const base = displayName.replace(/(?:\[[0-9]+\])+$/, '')
-  const suffixCount = '[' + tootCount.toString() + ']'
 
 
-  const countArea = document.createElement('p')
-  countArea.style.display = 'none'
-  countArea.className = 'toot-count'
-  countArea.innerText = tootCount.toString()
-  document.body.appendChild(countArea)
-
-
-  const update_display_name = (name, token) => {
-    const host = location.origin + '/'
-    const update = host + 'api/v1/accounts/update_credentials'
-
-    const xhr = new XMLHttpRequest()
-
-    xhr.open('PATCH', update)
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    xhr.timeout = '3000'
-
-    const patch = 'display_name=' + encodeURIComponent(name).replace(/%20/, '+')
-    xhr.send(patch)
-  }
-
-  update_display_name(base + suffixCount, bearerToken)
+  let msg = browser.runtime.sendMessage(
+    {
+      'token': bearerToken,
+      'host': location.origin + '/',
+      'base': base
+    }
+  )
 })()
